42ae252f1030dafe9da633a16d35e4f150922802
Luis Romba <l.romba@campus.fct.unl.pt>
2022-04-20 19:41:24 +0100

    position_tested

--- Position.py
+++ Position.py
@@ -2,16 +2,23 @@ from __future__ import annotations
 
 
 class Position:
-    _pos = tuple[int, int]
+    _pos = (int, int)
 
     def __init__(self, row: int, col: int):
-        pass
+        if((type(row) is int or type(row) is float) and row >= 0  and (type(col) is int or type(col) is float) and col >= 0):
+            self._pos = (int(row),int(col))
+        else:
+            raise ValueError('__init__() invalid arguments')
+      
 
     def __str__(self):
-        pass
+        return '(' + str(self._pos[0]) + ', ' + str(self._pos[1]) + ')'
 
     def __getitem__(self, item: int) -> int:
-        pass
+        if(item >= 0 and item <= 1):
+            return self._pos[item]
+        else:
+            raise ValueError('__getitem__() invalid arguments')
 
     def __eq__(self, other: Position):
-        passx
+        return self._pos == other
